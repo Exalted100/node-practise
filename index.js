96f527953c29dad19f32a6605c728e3e52bc7a81
Fawaz Haroun <harounayomikun@gmail.com>
2021-02-13 14:58:22 +0100

    more development

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 const { Schema } = mongoose;
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
+
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new Schema({
-    name: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: String,
     age: Number,
     email: String,
     password: String
@@ -20,6 +24,17 @@ const homePage = __dirname + "/view/index.html";
 
 app.get("/", (req, res) => {
     res.sendFile(homePage)
+})
+
+app.post("/", (req, res) => {
+    let p;
+    p = new User(req.body);
+    /*p.save(function(err, data) {
+        if (err) return console.error(err);
+        done(null, data)
+      })*/
+    res.send(p)
+    console.log(p)
 })
 
 let PORT = process.env.PORT || 3000;
--- view/index.js
+++ view/index.js
@@ -1,12 +1,21 @@
 import axios from "axois";
 
-axios({
-    method: 'post',
-    url: '/signup',
-    data: {
-      firstName: 'Finn',
-      lastName: 'Williams'
-    }
-  });
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const age = document.querySelector("#age");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-document.querySelector("button").addEventListener("onclick", )
+/*const apiRequest = () => {
+    axios.post('/signup', {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      age: age.value,
+      email: email.value,
+      password: password.value
+    })
+  .then((response) => document.querySelector("h6").innerHTML = response)
+  .then(response => console.log(response))
+}
+
+document.querySelector("button").addEventListener("click", apiRequest);*/
